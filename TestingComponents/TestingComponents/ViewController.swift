5d875ff8faf578b3d6c043f14455245b0aa152a3
Keval <keval.langalia@live.com>
2021-03-09 09:33:22 +0530

    configured picker view in Testing components project

--- TestingComponents/TestingComponents/ViewController.swift
+++ TestingComponents/TestingComponents/ViewController.swift
@@ -10,16 +10,25 @@ import UIKit
 class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
     
     @IBOutlet weak var pickerManuf: UIPickerView!
-    @IBOutlet weak var pickerModel: UIPickerView!
+    // @IBOutlet weak var pickerModel: UIPickerView!
     @IBOutlet weak var lablePrice: UILabel!
     
-//    var data: [String, [String]] = [
-//    "abc" : ["xyz", "pqr"]
+//    var data: [String : Array<String>] = [
+//        "Lamborghini" : ["Countach", "Gallardo", "Aventador"],
+//        "Ford" : ["GT", "Fiesta", "Figo"],
+//        "Porche" : ["Carrera GT", "911 Carrera"],
+//        "Audi" : ["R8", "Spider"]
 //    ]
+    
+    var manufacturers = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        pickerManuf.dataSource = self
+        pickerManuf.delegate = self
+        
+        manufacturers.append(contentsOf: ["Lamborghini", "Ford", "Porche", "Audi"])
     }
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
@@ -27,12 +36,15 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        if pickerView == pickerManuf {
-            // add code here
-        }
-        else {
-            
-        }
+        return manufacturers.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return manufacturers[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        lablePrice.text = manufacturers[row]
     }
     
 }
